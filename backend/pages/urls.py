5a5d96eb7d0e81469f77ab420df8ab6e6eaf86a1
Artem <shenlin@yandex.ru>
2024-03-06 23:38:49 +0300

    add single page view

--- backend/pages/urls.py
+++ backend/pages/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path("", views.get_pages, name="get_pages"),
+    path("<str:page_id>", views.get_page, name="get_page"),
 ]
--- backend/pages/views.py
+++ backend/pages/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import JsonResponse
 from pages.models import Page
 
@@ -16,4 +16,19 @@ def get_pages(request):
         })
     return JsonResponse(
         pages_list, safe=False
+    )
+
+
+def get_page(request, page_id):
+    page = get_object_or_404(Page, id=page_id)
+    serialized_page = {
+        "id": page.id,
+        "title": page.title,
+        "description": page.description,
+        "h1": page.h1,
+        "created_at": page.created_at,
+        "updated_at": page.updated_at
+    }
+    return JsonResponse(
+        serialized_page, safe=False
     )
