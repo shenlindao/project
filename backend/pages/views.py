962d395c75fc82ff7b1a4bfd1e44a112c443881e
Artem <shenlin@yandex.ru>
2024-03-06 23:25:35 +0300

    add pages view

--- backend/pages/views.py
+++ backend/pages/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from pages.models import Page
 
-# Create your views here.
+def get_pages(request):
+    pages = Page.objects.all() # взяли все объекты
+    pages_list = [] # создали пустой список
+    for page in pages:
+        pages_list.append({
+            "id": page.id,
+            "title": page.title,
+            "description": page.description,
+            "h1": page.h1,
+            "created_at": page.created_at,
+            "updated_at": page.updated_at
+        })
+    return JsonResponse(
+        pages_list, safe=False
+    )
